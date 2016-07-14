42db20bff37fdfeb63c57a6ae1fa4b536e5d14ba
nicoletafusea <nicoleta_8481@yahoo.com>
2016-07-14 19:20:46 +0300

    MVC Day 1 continuare

---
+++ JavaMvcApp/src/main/java/ro/teamnet/zth/appl/controller/LocationController.java
@@ -0,0 +1,20 @@
+package ro.teamnet.zth.appl.controller;
+
+import ro.teamnet.zth.api.annotations.MyController;
+import ro.teamnet.zth.api.annotations.MyRequestMethod;
+
+/**
+ * Created by nicoleta on 7/14/2016.
+ */
+@MyController(urlPath = "/locations")
+public class LocationController {
+    @MyRequestMethod(urlPath = "/all")
+    public String getAllLocations() {
+        return "allLocations";
+    }
+
+    @MyRequestMethod(urlPath = "/one")
+    public String getOneLocation() {
+        return "oneRandomLocation";
+    }
+}
